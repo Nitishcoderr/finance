09d3e7dfa95c78df486b9e4266d48039dfc07335
Your Name <nitishchaurasiyac555@gmail.com>
2025-04-30 00:31:27 +0530

    changing of default account completed

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client"
+
+import React, { useEffect } from 'react';
 import {
   Card,
   CardContent,
@@ -11,14 +13,46 @@ import { Switch } from '@/components/ui/switch';
 import { ArrowUpRight } from 'lucide-react';
 import { ArrowDownRight } from 'lucide-react';
 import Link from 'next/link';
+import UseFetch from '@/hooks/use-fetch';
+import { updateDefaultAccount } from '@/actions/accounts';
+import { toast } from 'sonner';
 const AccountCard = ({ account }) => {
   const { name, type, balance, id, isDefault } = account;
+
+  const {loading:updateDefaultLoading,
+    fn:updateDefaultFn,
+    data:updateAccount,
+    error,
+  } =UseFetch(updateDefaultAccount)
+
+  const handleDefaultChange = async(event)=>{
+    event.preventDefault()
+    if(isDefault){
+        toast.warning("You need atleast 1 default account")
+        return;
+    }
+    await updateDefaultFn(id)
+  }
+
+  useEffect(() => {
+   if(updateAccount?.success){
+    toast.success("Default account updated successfully")
+   }
+  }, [updateAccount,updateDefaultLoading])
+  
+  useEffect(() => {
+   if(error){
+    toast.error(error.message || "Failed to update default account")
+   }
+  }, [error])
+  
+
   return (
     <Card className="hover:shadow-md transition-shadow group relative" >
         <Link href={`/account/${id}`} >
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2" >
         <CardTitle className="text-sm font-medium capitalize">{name}</CardTitle>
-        <Switch checked={isDefault}  />
+        <Switch onClick={handleDefaultChange} checked={isDefault} disabled={updateDefaultLoading}  />
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">${parseFloat(balance).toFixed(2)}</div>
